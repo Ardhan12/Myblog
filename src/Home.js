204973b686c6eba22d30b412c690819c7c651f0a
Ardhan12 <arief.ramadhan43@gmail.com>
2022-11-25 00:01:02 +0700

    add button

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import { useState } from "react"
+
 const Home = () => {
+    const [name, setName] = useState("aripp")
+    const [age, setAge] = useState(23)
+
+    const handleClick = () => {
+        setName("adellaaaa")
+        setAge(17)
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }

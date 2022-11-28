5fc02a61fa56e316fb8d5ce8c1b57c88b545d643
Ardhan12 <arief.ramadhan43@gmail.com>
2022-11-28 08:04:45 +0700

    add context

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,35 @@
 import { useState } from "react"
 
 const Home = () => {
-    const [name, setName] = useState("aripp")
-    const [age, setAge] = useState(23)
-
-    const handleClick = () => {
-        setName("adellaaaa")
-        setAge(17)
-    }
+    const[blogs, setBlogs] = useState([
+        {title: "My New Website", body: "lorem ipsum ...", author: "ardhan", id: 1},
+        {title: "Welcome to Party", body: "lorem ipsum ...", author: "ardhan", id: 2},
+        {title: "Web dev top tips", body: "lorem ipsum ...", author: "ardhan", id: 3},
+    ])
+    
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
  
 export default Home;
+
+/*
+const [name, setName] = useState("aripp")
+    const [age, setAge] = useState(23)
+
+    const handleClick = () => {
+        setName("adellaaaa")
+        setAge(17)
+    } 
+    <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
+        
+    */
